5fc2b9e173844f9edbc37421b6cfb6b14d5063a8
jemmj <72713146+jemmj@users.noreply.github.com>
2020-11-03 11:04:45 +0100

    template
    update

--- ajax/formulaire.php
+++ ajax/formulaire.php
@@ -1,23 +1,35 @@
-<<<<<<< Updated upstream
-=======
+  
+<link rel="stylesheet"     href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384- Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
 
->>>>>>> Stashed changes
+  <link rel="stylesheet" href="formulaire.css"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <!-- Formulaire HTML super simple à sérialiser -->
 
+<div class="container">
+<div class="row">
+	<div class="col-4"></div>
+	<div class="col-4">
+		
+<form class="form-group"id="formulaire" method="POST" action="traitement.php">
+<label for="exampleInputNom">Name</label>
+<input class="form-control" type="text" placeholder="Enter your Name" name="nom" /><br/>
+<label for="exampleInputPrenom">Surname</label>
+<input class="form-control"  type="text" placeholder="Enter your Surname" name="prenom" /> <br/>
+ <label for="exampleInputEmail1">Email address</label>
+<input class="form-control" type="text" placeholder="Enter your Email adress" name="email" id="email" /> <span id='error_email' style="color:red"> </span><br/>
+<label for="exampleInputPassword">Password</label>
+<input class="form-control" type="text" placeholder="Password"  name="password" id="password" /><span id='error_password' style="color:red"> </span><br/>
+<label for="exampleInputRetypepassword">Retypepassword</label>
+<input class="form-control" type="text" placeholder="RetypePassword" name="repassword" id="repassword" /><span id='error_retypepassword' style="color:red"> </span><br/>
 
-<form id="formulaire" method="POST" action="traitement.php">
-Nom<input type="text" name="nom" /><br/>
-Prenom<input type="text" name="prenom" /> <br/>
-Email<input type="text" name="email" id="email" /> <span id='error_email' style="color:red"> </span><br/>
-Password<input type="text" name="password" id="password" /><span id='error_password' style="color:red"> </span><br/>
-Retypepassword<input type="text" name="repassword" id="repassword" /><span id='error_retypepassword' style="color:red"> </span><br/>
-
-<input type="submit" name="submit" />
+<input type="submit"class="btn btn-primary btn-block btn-sm" name="submit" />
 </form>
-
+</div>
+</div>
+</div>
+</div>
 
 
 <script>
